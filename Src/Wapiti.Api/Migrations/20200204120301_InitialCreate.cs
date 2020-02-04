f5a13fba99e38905b3aa254c609594fba6333898
Tomas Deceuninck <tomas.deceuninck@dexmach.com>
2020-02-04 12:04:33 +0000

    Initial MTGCollection.db using model

---
+++ Src/Wapiti.Api/Migrations/20200204120301_InitialCreate.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wapiti.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Collections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CollectionCards",
+                columns: table => new
+                {
+                    CardName = table.Column<string>(nullable: false),
+                    CollectionId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollectionCards", x => new { x.CollectionId, x.CardName });
+                    table.ForeignKey(
+                        name: "FK_CollectionCards_Cards_CardName",
+                        column: x => x.CardName,
+                        principalTable: "Cards",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CollectionCards_Collections_CollectionId",
+                        column: x => x.CollectionId,
+                        principalTable: "Collections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Decks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    CollectionId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Decks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Decks_Collections_CollectionId",
+                        column: x => x.CollectionId,
+                        principalTable: "Collections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeckCards",
+                columns: table => new
+                {
+                    CardName = table.Column<string>(nullable: false),
+                    DeckId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeckCards", x => new { x.DeckId, x.CardName });
+                    table.ForeignKey(
+                        name: "FK_DeckCards_Cards_CardName",
+                        column: x => x.CardName,
+                        principalTable: "Cards",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeckCards_Decks_DeckId",
+                        column: x => x.DeckId,
+                        principalTable: "Decks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollectionCards_CardName",
+                table: "CollectionCards",
+                column: "CardName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeckCards_CardName",
+                table: "DeckCards",
+                column: "CardName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Decks_CollectionId",
+                table: "Decks",
+                column: "CollectionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollectionCards");
+
+            migrationBuilder.DropTable(
+                name: "DeckCards");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Decks");
+
+            migrationBuilder.DropTable(
+                name: "Collections");
+        }
+    }
+}
